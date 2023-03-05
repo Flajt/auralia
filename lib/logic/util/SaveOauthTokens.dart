39f4317abc4acdca2e0c54fcbcc2b1a5a9b2bded
Flajt <matrimkurz@gmail.com>
2023-03-05 19:59:52 +0100

    Fix: Check for empty tokens beforehand

--- lib/logic/util/SaveOauthTokens.dart
+++ lib/logic/util/SaveOauthTokens.dart
@@ -6,9 +6,11 @@ import '../services/SecureStorageWrapperService.dart';
 
 void saveOauthTokens(AsyncSnapshot<AuthState> snapshot) {
   String jwt = snapshot.data!.session!.accessToken;
-  String refreshToken = snapshot.data!.session!.providerRefreshToken!;
-  String providerToken = snapshot.data!.session!.providerToken!;
-  SpotifyOauthKeyService(
-          jwt: jwt, storageWrapperService: SecureStorageWrapperService())
-      .putAccessTokens(providerToken, refreshToken);
+  String? refreshToken = snapshot.data!.session!.providerRefreshToken;
+  if (refreshToken != null) {
+    String providerToken = snapshot.data!.session!.providerToken!;
+    SpotifyOauthKeyService(
+            jwt: jwt, storageWrapperService: SecureStorageWrapperService())
+        .putAccessTokens(providerToken, refreshToken);
+  }
 }
