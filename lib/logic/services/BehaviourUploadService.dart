ab6351fd69be050ae6e2ac7b2ceebf3ba3b00733
Flajt <matrimkurz@gmail.com>
2023-03-06 15:08:44 +0100

    Fix+Feat: background service setup , refactor,cleanup

--- lib/logic/services/BehaviourUploadService.dart
+++ lib/logic/services/BehaviourUploadService.dart
@@ -1,38 +1,48 @@
 import 'dart:convert';
 
 import 'package:auralia/logic/abstract/DBServiceA.dart';
+import 'package:auralia/logic/util/toUtcTime.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import "package:http/http.dart" as http;
 import '../../models/regular/ListeningBehaviourModel.dart';
 
-class BehaviourUploadServices {
+class BehaviourUploadService {
   final DBServiceA dbServiceA;
   final String baseUrl;
+  final String jwt;
   //TODO: Create provider with basic values
-  const BehaviourUploadServices(
-      {required this.dbServiceA, this.baseUrl = "http://192.168.0.6:8000"});
+  const BehaviourUploadService(
+      {required this.dbServiceA,
+      required this.jwt,
+      this.baseUrl = "http://192.168.0.6:8000"});
 
   Future<int> get recentUploadTime async =>
       (await SharedPreferences.getInstance()).getInt("songTime") ??
-      DateTime.now().toUtc().millisecondsSinceEpoch;
+      toUtcTime(DateTime.now()).millisecondsSinceEpoch;
 
   Future<void> setRecentUploadTime(DateTime recentUploadTime) async {
     final instance = await SharedPreferences.getInstance();
     instance.setInt(
-        "songTime", recentUploadTime.toUtc().millisecondsSinceEpoch);
+        "songTime", toUtcTime(recentUploadTime).millisecondsSinceEpoch);
   }
 
   Future<void> uploadSongs() async {
+    final lastUploaded = await recentUploadTime;
     List<ListeningBehaviourModel> listeningBehaviour =
-        await dbServiceA.getRecent(await recentUploadTime);
+        await dbServiceA.getRecent(lastUploaded);
     List<Map<String, dynamic>> jsonModels = [];
     for (ListeningBehaviourModel behaviour in listeningBehaviour) {
       jsonModels.add(behaviour.toJson());
     }
     if (jsonModels.isNotEmpty) {
-      DateTime now = DateTime.now();
+      DateTime now = DateTime.now().toUtc();
+      String encodedBody = jsonEncode({"behaviour": jsonModels});
       http.Response resp = await http.post(Uri.parse("$baseUrl/add-songs"),
-          body: jsonEncode({"behaviour": jsonModels}));
+          headers: {
+            "Authorization": "Bearer $jwt",
+            "Content-Type": "application/json"
+          },
+          body: encodedBody);
       if (resp.statusCode < 400) {
         await setRecentUploadTime(now);
       } else {
---
+++ lib/logic/util/toUtcTime.dart
@@ -0,0 +1,1 @@
+DateTime toUtcTime(DateTime dateTime) => dateTime.toUtc();
