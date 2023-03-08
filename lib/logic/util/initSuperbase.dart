682a72b1aadff993a58dd3855ddd3c2bec1a9397
Flajt <matrimkurz@gmail.com>
2023-03-08 13:25:24 +0100

    Fix: Late initialize error on release

--- lib/logic/util/initSuperbase.dart
+++ lib/logic/util/initSuperbase.dart
@@ -1,12 +1,14 @@
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<Supabase> initSupabase() async {
   try {
-    return Supabase.instance;
-  } catch (e) {
-    return Supabase.initialize(
+    return await Supabase.initialize(
         url: "https://limbadcemvavrnorbkig.supabase.co",
         anonKey:
             "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxpbWJhZGNlbXZhdnJub3Jia2lnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzY5ODYzNTYsImV4cCI6MTk5MjU2MjM1Nn0.RgD8isCOgvIADI9Yv9iifhFi1grpwzZYP-BGIeXXzJM");
+  } catch (e, stack) {
+    await Sentry.captureException(e, stackTrace: stack, hint: "Inside");
+    return Supabase.instance;
   }
 }
