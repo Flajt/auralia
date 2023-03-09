3083402e508542eabd3952c75f87d85c0dfbcbd5
Flajt <matrimkurz@gmail.com>
2023-03-09 13:49:04 +0100

    Fix: make variable optional, remove broken null check

--- lib/logic/util/initSentry.dart
+++ lib/logic/util/initSentry.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/foundation.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 
-Future<void> initSentry(VoidCallback? runApp) async {
+Future<void> initSentry([VoidCallback? runApp]) async {
   await SentryFlutter.init((p0) {
     p0.dsn = kDebugMode
         ? ""
         : "https://b2410170c0db436ea1eac986dc20ff22@o517360.ingest.sentry.io/4504797082288128";
     p0.tracesSampleRate = .5;
-  }, appRunner: runApp ?? runApp);
+  }, appRunner: runApp);
 }
