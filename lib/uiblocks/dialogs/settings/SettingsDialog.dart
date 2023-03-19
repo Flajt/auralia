b7b59ba748c3ef50f25475dab28e746a62e761d7
Flajt <matrimkurz@gmail.com>
2023-03-19 16:41:32 +0100

    Refactor: Refactor SettingsDialog to use Bloc

--- lib/uiblocks/dialogs/settings/SettingsDialog.dart
+++ lib/uiblocks/dialogs/settings/SettingsDialog.dart
@@ -1,76 +1,78 @@
-import 'package:auralia/logic/services/BehaviourUploadService.dart';
-import 'package:auralia/logic/services/DBService.dart';
+import 'package:auralia/bloc/SettingsBloc/SettingsBlocEvent.dart';
 import 'package:elegant_notification/elegant_notification.dart';
 import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../bloc/SettingsBloc/SettingsBloc.dart';
+import '../../../bloc/SettingsBloc/SettingsBlocState.dart';
 
 class SettingsDialog extends StatelessWidget {
   const SettingsDialog({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    String jwt = Supabase.instance.client.auth.currentSession!.accessToken;
-    return Dialog(
-      child: Container(
-          padding: const EdgeInsets.all(8.0),
-          width: 400,
-          height: 400,
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text("Settings",
-                    style: Theme.of(context)
-                        .textTheme
-                        .titleLarge
-                        ?.copyWith(fontWeight: FontWeight.bold)),
-              ),
-              Expanded(
-                child: ListView(
-                  children: [
-                    ListTile(
-                      title: const Text("Listening Behaviour"),
-                      onTap: () => Navigator.of(context)
-                          .pushNamed("/listeningBehaviour"),
-                    ),
-                    ListTile(
-                      title: const Text("Upload History"),
-                      onTap: () async {
-                        try {
-                          await BehaviourUploadService(
-                                  dbServiceA: IsarDBService(),
-                                  jwt: jwt,
-                                  baseUrl: "https://auralia.fly.dev")
-                              .uploadSongs();
-                          // ignore: use_build_context_synchronously
-                          ElegantNotification.success(
-                                  description:
-                                      const Text("Data successfully uploaded"))
-                              .show(context);
-                        } catch (e) {
-                          ElegantNotification.error(
-                                  description: Text(e.toString()))
-                              .show(context);
-                        }
-                      },
-                    ),
-                    ListTile(
-                        title: const Text("Logout"),
-                        onTap: () {
-                          Navigator.of(context).pop();
-                          Supabase.instance.client.auth.signOut();
-                        }),
-                    const IgnorePointer(
-                      child: ListTile(
-                        title: Text("Delete Account"),
-                      ),
-                    ),
-                    const AboutListTile(),
-                  ],
+    return BlocListener<SettingsBloc, SettingsBlocState>(
+      listenWhen: (prev, cur) =>
+          cur is ErrorUploadingBehaviour || cur is SuccessUploadingBehaviour
+              ? true
+              : false,
+      listener: (context, state) {
+        if (state is ErrorUploadingBehaviour) {
+          ElegantNotification.error(
+                  description: Text(state.errorMsg.toString()))
+              .show(context);
+        } else if (state is SuccessUploadingBehaviour) {
+          ElegantNotification.success(
+                  description: const Text("Data successfully uploaded"))
+              .show(context);
+        }
+      },
+      child: Dialog(
+        child: Container(
+            padding: const EdgeInsets.all(8.0),
+            width: 400,
+            height: 400,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text("Settings",
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleLarge
+                          ?.copyWith(fontWeight: FontWeight.bold)),
                 ),
-              ),
-            ],
-          )),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      ListTile(
+                        title: const Text("Listening Behaviour"),
+                        onTap: () => Navigator.of(context)
+                            .pushNamed("/listeningBehaviour"),
+                      ),
+                      ListTile(
+                          title: const Text("Upload History"),
+                          onTap: () => context
+                              .read<SettingsBloc>()
+                              .add(UploadBehaviour())),
+                      ListTile(
+                          title: const Text("Logout"),
+                          onTap: () {
+                            Navigator.of(context).pop();
+                            context.read<SettingsBloc>().add(LogOut());
+                          }),
+                      const IgnorePointer(
+                        child: ListTile(
+                          title: Text("Delete Account"),
+                        ),
+                      ),
+                      const AboutListTile(),
+                    ],
+                  ),
+                ),
+              ],
+            )),
+      ),
     );
   }
 }
