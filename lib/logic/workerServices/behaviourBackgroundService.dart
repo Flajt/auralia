4d3493a1ea8727cbe9df849dacf3150fe6de49bf
Flajt <matrimkurz@gmail.com>
2023-03-20 18:57:01 +0100

    Refactor: Apply changes due to related classes

--- lib/logic/workerServices/behaviourBackgroundService.dart
+++ lib/logic/workerServices/behaviourBackgroundService.dart
@@ -3,7 +3,7 @@ import 'package:auralia/logic/services/BehaviourUploadService.dart';
 import 'package:auralia/logic/services/DBService.dart';
 import 'package:auralia/logic/util/InternetUtil.dart';
 import 'package:auralia/logic/util/initSentry.dart';
-import 'package:auralia/logic/util/initSuperbase.dart';
+import 'package:auralia/logic/util/registerServices.dart';
 import 'package:get_it/get_it.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:workmanager/workmanager.dart';
@@ -12,18 +12,17 @@ import 'package:workmanager/workmanager.dart';
 Future<void> behaviourBackgroundService() async {
   Workmanager().executeTask((taskName, inputData) async {
     try {
+      await registerServices();
       final hasNet = await InternetUtil.hasInternet();
       final GetIt getIt = GetIt.I;
       getIt.registerSingleton<DBServiceA>(IsarDBService());
       await initSentry();
       await Sentry.addBreadcrumb(Breadcrumb(
-          message: "behaviourBackgroundService before initSupabase",
+          message: "behaviourBackgroundService before upload",
           data: {"hasInternet": hasNet},
           level: SentryLevel.info));
-      final supabase = await initSupabase();
-      final accessToken = supabase.client.auth.currentSession!.accessToken;
       final uploadService = BehaviourUploadService(
-          getIt: getIt, jwt: accessToken, baseUrl: "https://auralia.fly.dev");
+          getIt: getIt, baseUrl: "https://auralia.fly.dev");
       await uploadService.uploadSongs();
       return Future.value(true);
     } catch (e, stack) {
