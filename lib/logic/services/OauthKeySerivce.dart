edb64f81debe5520b1bf72cdb15c22c20602f2ca
Flajt <matrimkurz@gmail.com>
2023-03-20 18:49:50 +0100

    Refactor: OauthKeyService

--- lib/logic/services/OauthKeySerivce.dart
+++ lib/logic/services/OauthKeySerivce.dart
@@ -1,35 +1,38 @@
 import 'dart:convert';
-import 'package:auralia/logic/services/SecureStorageWrapperService.dart';
+import 'package:auralia/logic/abstract/SecureStorageServiceA.dart';
+import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
 
-class SpotifyOauthKeyService {
-  late final SecureStorageWrapperService _storageWrapperService;
-  late final String _jwt;
+import '../abstract/OauthKeyServiceA.dart';
+
+class SpotifyOauthKeyService implements OauthKeyServiceA {
+  late final SecureStorageServiceA _storageWrapperService;
   final String baseUrl;
-  SpotifyOauthKeyService(
-      {required String jwt,
-      required SecureStorageWrapperService storageWrapperService,
-      this.baseUrl = "http://192.168.0.6:8000"}) {
-    _jwt = jwt;
-    _storageWrapperService = storageWrapperService;
+  final _getIt = GetIt.I;
+  SpotifyOauthKeyService({this.baseUrl = "http://192.168.0.6:8000"}) {
+    _storageWrapperService = _getIt<SecureStorageServiceA>();
   }
 
+  @override
   Future<void> putAccessTokens(
       String providerToken, String refreshToken) async {
     await _storageWrapperService.write("refreshToken", refreshToken);
     await _storageWrapperService.write("providerToken", providerToken);
   }
 
+  @override
   Future<String?> get accessToken async =>
       await _storageWrapperService.read("providerToken");
+  @override
   Future<String?> get refreshToken async =>
       _storageWrapperService.read("refreshToken");
 
-  Future<void> updateAccessToken() async {
+  @override
+  Future<void> updateAccessToken(String jwt) async {
     String? refToken = await refreshToken;
     http.Response resp = await http.get(Uri.parse("$baseUrl/update-token"),
         headers: {
-          "Authorization": "Bearer $_jwt",
+          "Authorization": "Bearer $jwt",
           "X-Refresh-Token": refToken!
         });
     if (resp.statusCode < 400) {
