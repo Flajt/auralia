c3f9ce2e06b7d6b157643e03217bfff27c3cb7ba
Flajt <matrimkurz@gmail.com>
2023-03-20 18:54:06 +0100

    Refactor: Clean up

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,21 @@
 import 'dart:io';
-
-import 'package:auralia/logic/abstract/AuthServiceA.dart';
-import 'package:auralia/logic/abstract/DBServiceA.dart';
-import 'package:auralia/logic/services/AuthService.dart';
-import 'package:auralia/logic/services/DBService.dart';
 import 'package:auralia/logic/util/initSentry.dart';
-import 'package:auralia/logic/webview/SpotifyChoreSafariBrowser.dart';
 import 'package:auralia/logic/workerServices/behaviourBackgroundService.dart';
 import 'package:auralia/pages/HomePage.dart';
 import 'package:auralia/pages/LoginPage.dart';
 import 'package:auralia/pages/UserBehaviourPage.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
-import 'package:get_it/get_it.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:workmanager/workmanager.dart';
 import 'logic/util/SaveOauthTokens.dart';
-import 'logic/util/initSuperbase.dart';
+import 'logic/util/registerServices.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final GetIt getIt = GetIt.I;
-  getIt.registerSingleton<AuthServiceA>(AuthService());
-  await getIt<AuthServiceA>().init();
-  getIt
-      .registerFactory<ChromeSafariBrowser>(() => SpotifyChromeSafariBrowser());
-  getIt.registerLazySingleton<DBServiceA>(() => IsarDBService());
+
+  ///TODO: Refactor into setup function
+  await registerServices();
   if (Platform.isAndroid) {
     await Workmanager()
         .initialize(behaviourBackgroundService, isInDebugMode: true);
@@ -37,7 +26,6 @@ Future<void> main() async {
         initialDelay: const Duration(minutes: 5),
         backoffPolicy: BackoffPolicy.exponential);
   }
-  await initSupabase();
   await initSentry(() => runApp(const MyApp()));
 }
 
