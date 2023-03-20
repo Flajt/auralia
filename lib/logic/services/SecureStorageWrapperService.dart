1def8e3f71a0453a0f6d19bf8b9dc9cadac291dd
Flajt <matrimkurz@gmail.com>
2023-03-20 18:51:49 +0100

    Refactor: Impl interface

--- lib/logic/services/SecureStorageWrapperService.dart
+++ lib/logic/services/SecureStorageWrapperService.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-class SecureStorageWrapperService {
+import '../abstract/SecureStorageServiceA.dart';
+
+class SecureStorageWrapperService implements SecureStorageServiceA {
   late final FlutterSecureStorage _secureStorage;
   SecureStorageWrapperService() {
     //See offical package readme
@@ -12,18 +14,22 @@ class SecureStorageWrapperService {
   AndroidOptions _getAndroidOptions() => const AndroidOptions(
         encryptedSharedPreferences: true,
       );
+  @override
   Future<String?> read(String key) async {
     return await _secureStorage.read(key: key);
   }
 
+  @override
   Future<void> write(String key, String value) async {
     await _secureStorage.write(key: key, value: value);
   }
 
+  @override
   Future<void> delete(String key) async {
     await _secureStorage.delete(key: key);
   }
 
+  @override
   Future<void> deleteAll() async {
     await _secureStorage.deleteAll();
   }
