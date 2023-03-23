39e920db460a0416b856a736ce6ad62f2ab54de5
Flajt <matrimkurz@gmail.com>
2023-03-23 15:24:54 +0100

    Feat: Impl PlayerWidget & migrate to Stateless Widget

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,60 +1,23 @@
-import 'dart:async';
 import 'package:auralia/bloc/CollectionForegroundBloc/CollectionForegroundServiceBloc.dart';
 import 'package:auralia/bloc/PermissionBloc/PermissionBloc.dart';
 import 'package:auralia/logic/services/PermissionService.dart';
-import 'package:auralia/logic/util/InternetUtil.dart';
 import 'package:auralia/uiblocks/buttons/SettingsButton.dart';
 import 'package:auralia/uiblocks/buttons/personalization/PersonalizationButton.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 import 'package:get_it/get_it.dart';
-import 'package:internet_connection_checker/internet_connection_checker.dart';
-import 'package:sentry_flutter/sentry_flutter.dart';
-
 import '../logic/abstract/PermissionServiceA.dart';
+import '../uiblocks/playerControlls/Player.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  late final StreamSubscription _netSubscription;
-  _HomePageState() {
-    _netSubscription =
-        InternetUtil.connectionStateStream().listen((event) async {
-      bool hasNet = event == InternetConnectionStatus.connected;
-      await Sentry.addBreadcrumb(Breadcrumb(
-          message: "HomePage Internet Check",
-          data: {"hasNet": hasNet},
-          level: SentryLevel.info));
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _netSubscription.cancel();
-    super.dispose();
-  }
-
-  @override
-  void didChangeDependencies() {
+  build(BuildContext context) {
     if (GetIt.I.isRegistered<PermissionServiceA>() == false) {
       GetIt.I.registerSingleton<PermissionServiceA>(PermissionService(context));
     }
-    super.didChangeDependencies();
-  }
-
-  @override
-  Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return WithForegroundTask(
       child: Scaffold(
@@ -64,11 +27,7 @@ class _HomePageState extends State<HomePage> {
                 child: Stack(children: [
                   const Align(
                       alignment: Alignment.topRight, child: SettingsButton()),
-                  Center(
-                      child: Text(
-                          "This app currently only collects data, more features comming soon.",
-                          style: Theme.of(context).textTheme.headlineMedium,
-                          textAlign: TextAlign.center)),
+                  const Center(child: Player()),
                   Align(
                       alignment: Alignment.bottomCenter,
                       child: MultiBlocProvider(
@@ -78,7 +37,7 @@ class _HomePageState extends State<HomePage> {
                               create: (context) => CollectionForegroundBloc())
                         ],
                         child: const PersonalizationButton(),
-                      ))
+                      )),
                 ]))),
       ),
     );
